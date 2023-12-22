bbe0c5afcc866de4d235825e3dd92752e1e7442d
SKM <rkdalsv@naver.com>
2023-12-23 00:55:09 +0900

    확인버튼 누르면 문장반영

--- app/src/main/java/com/example/uipractice/MainActivity.kt
+++ app/src/main/java/com/example/uipractice/MainActivity.kt
@@ -10,5 +10,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //확인버튼 눌리면 이벤트
+        binding.okBtn.setOnClickListener {
+//            입력된 내용 contentEditTxt (set) 변수에 저장
+            val inputContent = binding.contentEdit.text.toString()
+//            저장해둔 입력값을 텍스트뷰 내용으로  반영(get)
+            binding.resultTxt.text = inputContent
+
+        }
     }
 }
